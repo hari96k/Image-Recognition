efd3f2794783990d860b1b7d872122d7a0a3574e
Hari <hari96k@utexas.edu>
2017-06-16 02:59:15 -0500

    Patch

--- tf/tf_files/Vados.py
+++ tf/tf_files/Vados.py
@@ -1,18 +1,24 @@
+import base64
+import io
 import socket
 import threading
 import warnings
+
 import pytesseract
+import requests
+import scipy
+import scipy.cluster
+import scipy.misc
 import tensorflow as tf
 import webcolors
-import struct
-import scipy
-import scipy.misc
-import scipy.cluster
-
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 n_colors = 2
+
+# List of all colors:
+colors_set = {'#ffffff': 'white', '#000000': 'black', '#008000': 'green', '#808080': 'gray', '#0000ff': 'blue',
+              '#ffff00': 'yellow', '#ffa500': 'orange', '#ff0000': 'red', '#800080': 'purple', '#A52A2A': 'brown'}
 
 warnings.filterwarnings("ignore")
 
@@ -77,9 +83,10 @@ softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 #         print("Blob processed...")
 #         print("Active threads: " + str(threading.active_count()))
 
-def closest_colour(requested_colour):
+
+def get_colour_name(requested_colour):
     min_colours = {}
-    for key, name in webcolors.css21_hex_to_names.items():
+    for key, name in colors_set.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
         rd = (r_c - requested_colour[0]) ** 2
         gd = (g_c - requested_colour[1]) ** 2
@@ -88,15 +95,14 @@ def closest_colour(requested_colour):
     return min_colours[min(min_colours.keys())]
 
 
-def get_colour_name(requested_colour):
-    try:
-        closest_name = webcolors.rgb_to_name(requested_colour)
-    except ValueError:
-        closest_name = closest_colour(requested_colour)
-    return str(closest_name)
+# def get_colour_name(requested_colour):
+#     try:
+#         closest_name = webcolors.rgb_to_name(requested_colour)
+#     except ValueError:
+#         closest_name = closest_colour(requested_colour)
+#     return str(closest_name)
 
-
-def processBlob(data, directory):
+def process_blob(data, directory, imgID, latitude, longitude):
     m = str(data, "utf-8")
     m_split = m.split(' ')
     print("Matlab sent : " + m)
@@ -121,17 +127,23 @@ def processBlob(data, directory):
         result = ("%s with %s confidence" % (shape, confidence)).title()
         print('\n\n' + result + '\n' + str(m_split) + '\n')
         # Default is .85
-        if confidence > .85 and shape != 'nas':
+        if confidence > .90 and shape != 'nas':
+            w, h = cropped_img.size
+            cropped_img = cropped_img.crop((10, 10, w - 20, h - 20))
+            w, h = cropped_img.size
+            if w > 20 and h > 20:
+                inside_img = cropped_img.crop((5, 5, w - 10, h - 10))
+            else:
+                inside_img = cropped_img
             # cropped_img.show()
 
-            #### Alphanumeric detection
+            # Alphanumeric detection
             char = pytesseract.image_to_string(cropped_img, config='-psm 10')
             if not char.isalnum():
                 char = 'NaC'
 
-            #### Color detection
-
-            ar = scipy.misc.fromimage(cropped_img)
+            # Color detection
+            ar = scipy.misc.fromimage(inside_img)
             dim = ar.shape
             ar = ar.reshape(scipy.product(dim[:2]), dim[2])
             codes, dist = scipy.cluster.vq.kmeans(ar.astype(float), n_colors)
@@ -140,8 +152,32 @@ def processBlob(data, directory):
 
             cropped_img.save('C:\\Users\\Hari\\Documents\\UAV\\Image-Recognition\\tf\\tf_files\\outputs\\' + str(
                 shape) + '_' + char + '_' + primary + '_' + secondary + '_' + str(confidence) + '.jpg')
+            inside_img.save(
+                'C:\\Users\\Hari\\Documents\\UAV\\Image-Recognition\\tf\\tf_files\\outputs\\' + '_' + primary + '_' + secondary + '_' + str(
+                    confidence) + '.jpg')
 
-    # clientsock.send("You sent me : "+ str(data))
+            byte_array = io.BytesIO()
+            cropped_img.save(byte_array, format='PNG')
+            byte_array = byte_array.getvalue()
+            encoded_img = base64.encodebytes(byte_array)
+
+            dictionary = {'type': 'standard', 'data': str(encoded_img), 'latitude': latitude, 'longitude': longitude,
+                    'orientation': 'n', 'shape': shape, 'background_color': primary, 'alphanumeric_color': secondary,
+                    'autonomous': True}
+
+            # jsonString = json.dumps(dict)
+            # jsonString = "{\"data\":\"" + str(encoded_img) + "\",\"type\":\"standard\",\"lat\":" + "1" + ",\"lon\":" + "1" + ",\"orientation\":" + "\"N\"" + ",\"shape\":\"" + shape + "\",\"background_color\":\"" + primary + "\",\"alphanumeric\":\"" + char + "\",\"alphanumeric_color\":\"" + secondary + "\",\"autonomous\":true}"
+            # print(jsonString)
+            # output = json.loads(jsonString)
+
+            lol = {"processed": True}
+            # patchOut = json.dumps(lol)
+
+            patch_url = "http://192.168.0.13:25005/api/images/" + str(imgID)
+            url = "http://192.168.0.13:25005/api/targets/"
+            requests.patch(patch_url, json=lol)
+            print(requests.post(url, json=dictionary).text)
+            print(encoded_img)
     else:
         pass
 
@@ -177,9 +213,13 @@ while True:
             print(message)
             d = message_words[1]
             iterations = int(message_words[2])
+            img_id = int(message_words[3])
+            lat = float(message_words[4])
+            lon = float(message_words[5])
             for i in range(0, iterations):
                 data = clientsock.recv(100)
                 # newBlob = BlobThread(data, d)
                 # newBlob.start()
                 # executor.submit(processBlob, data, d)
-                processBlob(data, d)
+                process_blob(data, d, img_id, lat, lon)
+
